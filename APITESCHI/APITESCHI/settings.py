13972e1fa8077dedebbc674f9f2f4063e82ddd17
brayan121120 <brayangil1211@gmail.com>
2023-09-13 10:40:06 -0600

    Add

--- APITESCHI/APITESCHI/settings.py
+++ APITESCHI/APITESCHI/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'api'
-    'rest_framework'
+    'api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
