44ab95cb43d6c271e525c7de53d9af86272b0647
brayan121120 <brayangil1211@gmail.com>
2023-09-19 18:13:07 -0600

    ADD

--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework.views import A
+from rest_framework.views import APIView
 
 # Create your views here.
 class Home(APIView):
     template_name="index.html"
     def get(self,request):
-        return render(request,self.template_name)        
+        return render(request,self.template_name)       
